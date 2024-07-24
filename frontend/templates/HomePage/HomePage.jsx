34e0e70426707b958e0db843f87eafa45bf78245
EricLi3 <ericlibeibei@gmail.com>
2024-07-24 12:56:52 -0400

    SignInNotifs

--- frontend/templates/HomePage/HomePage.jsx
+++ frontend/templates/HomePage/HomePage.jsx
@@ -1,11 +1,57 @@
+import { useEffect } from 'react';
+
 import { Grid, Typography } from '@mui/material';
+
+import { useRouter } from 'next/router';
+import { useSelector } from 'react-redux';
+
+import { toast, ToastContainer } from 'react-toastify';
 
 import ToolsListingContainer from '@/components/ToolsListingContainer';
 
+// import { AUTH_CONTENT } from '@/constants/auth';
+// import ALERT_COLORS from '@/constants/notification';
+
 import styles from './styles';
+
+// import { AuthContext } from '@/providers/GlobalProvider';
 
 const HomePage = (props) => {
   const { data, loading } = props;
+
+  const { data: userData } = useSelector((state) => state.user);
+  const router = useRouter();
+  const userName = userData?.fullName;
+
+  useEffect(() => {
+    if (router.query.is_login === 'true') {
+      toast.success(
+        <>
+          <div>Log In Successful!</div>
+          <div>👋 Welcome Back! {userName || 'Anonymous'}</div>
+        </>,
+        {
+          position: 'top-right',
+          autoClose: 3000,
+        }
+      );
+      router.replace('/');
+    }
+
+    if (router.query.is_signup === 'true') {
+      toast.success(
+        <>
+          <div>Sign Up Successful!</div>
+          <div>👋 Welcome to KAI! {userName || 'Anonymous'}</div>
+        </>,
+        {
+          position: 'top-right',
+          autoClose: 3000,
+        }
+      );
+      router.replace('/');
+    }
+  }, [router.query, userName]);
 
   const renderTitle = () => {
     return (
@@ -23,14 +69,17 @@ const HomePage = (props) => {
   };
 
   return (
-    <Grid {...styles.mainGridProps}>
-      {renderTitle()}
-      <ToolsListingContainer
-        data={data}
-        loading={loading}
-        category="All Tools"
-      />
-    </Grid>
+    <>
+      <ToastContainer />
+      <Grid {...styles.mainGridProps}>
+        {renderTitle()}
+        <ToolsListingContainer
+          data={data}
+          loading={loading}
+          category="All Tools"
+        />
+      </Grid>
+    </>
   );
 };
 export default HomePage;
